b1caa4f8628a30973033b9259753cfb138968fbe
Nicole Laczny <nlaczn2@uic.edu>
2019-04-14 10:25:21 -0500

    Signed-off-by: Nicole Laczny <nlaczn2@uic.edu>

--- Project3/app.R
+++ Project3/app.R
@@ -6,6 +6,8 @@
 #
 #    http://shiny.rstudio.com/
 #
+################### key: 305d59068af82054adce3f22e00d7495
+
 
 library(shiny)
 library(AotClient) 
@@ -24,42 +26,113 @@ pollutants_list = list("NO2", "Ozone", "CO", "H2S", "SO2", "PM10", "PM25", "Temp
 ui <- dashboardPage(
   ################################################################################ THE COLOR AND LENGTH OF THE TITLE FOR THE SIDEBAR ################################################################################
   skin = "yellow",
-  dashboardHeader(title = "CS 424 PROJECT 2", titleWidth = 450 ),
+  dashboardHeader(title = "CS 424 PROJECT 2", titleWidth = 350 ),
   
   ######################################## CREATE DROP DOWN MENUS IN SIDEBAR + NEW TAB CONTAINING RESOURCES ######################################## 
-  dashboardSidebar(sidebarMenu(disable = FALSE, collapsed = FALSE,
+  dashboardSidebar(sidebarMenu(disable = FALSE, collapsed = FALSE,  style = "margin-top:500px",
                                uiOutput("nodeOutput"),
                                uiOutput("node1Output"),
                                uiOutput("node2Output"),
-                               selectInput("pollutants", "Pollutants", pollutants_list)
+                               selectInput("pollutants", "Pollutants", pollutants_list),
+                               menuItem("Node Map", tabName="map", icon = icon("dashboard")),
+                               menuItem("Comparison", tabName="compare", icon = icon("dashboard")),
+                               menuItem("Resources", tabName="resources", icon = icon("bullet")),
+                               menuItem("test", tabName="boxes", icon = icon("bullet")),
+                               checkboxInput("NO2", "NO2", TRUE),
+                               checkboxInput("OZONE", "OZONE", FALSE),
+                               checkboxInput("CO", "CO", FALSE),
+                               checkboxInput("H2S", "H2S", FALSE),
+                               checkboxInput("SO2", "SO2", FALSE),
+                               checkboxInput("PM10", "PM10", FALSE),
+                               checkboxInput("PM25", "PM25", FALSE),
+                               checkboxInput("TEMPERATURE", "TEMPERATURE", FALSE),
+                               checkboxInput("HUMIDITY", "HUMIDITY", FALSE),
+                               checkboxInput("INTENSITY", "INTENSITY", FALSE)
+                              
                               
   )),
   ######################################## THE MAIN BODDY OF THE WEB APP ########################################
   dashboardBody(
-    fluidRow(column(6,
+    tabItems(
+      tabItem(
+        tabName = "map",
+        fluidRow(column(6,
                     leafletOutput("mymap", height = 600)
                     ),
              column(6, h4(textOutput("Node Data")),
-                    tabsetPanel(
+                    tabsetPanel(type = "tabs",
                       tabPanel("Current",plotOutput("node_data")),
                       tabPanel("24 Hours", plotOutput("node_data24")),
-                      tabPanel("7 Days",plotOutput("node_data7"))
+                      tabPanel("7 Days",plotOutput("node_data7")),
+                      position = "left"
                     ))
-             ),
-    fluidRow(column(6,h4(textOutput("Node 1")),
-               tabsetPanel(
+             )),
+      tabItem(
+        tabName = "compare",
+        fluidRow(column(6,h4(textOutput("Node 1")),
+              
+               tabsetPanel(type = "tabs",
                  tabPanel("Current",plotOutput("node1_cur")),
                  tabPanel("24 Hours", plotOutput("node1_24")),
-                 tabPanel("7 Days",tableOutput("node1_7"))
+                 tabPanel("7 Days",tableOutput("node1_7")),
+                 position = "below"
                )
              ),
              column(6,h4(textOutput("Node 2")),
-                    tabsetPanel(
+                    tabsetPanel( type = "tabs",
                       tabPanel("Current",plotOutput("node2_cur")),
                       tabPanel("24 Hours", plotOutput("node2_24")),
-                      tabPanel("7 Days",plotOutput("node2_7"))
+                      tabPanel("7 Days",plotOutput("node2_7")),
+                      position = "left"
                     )
              )
+    )),
+    tabItem(
+      tabName = "resources",
+      h2("Resources used in this project:"),
+      h5("All data used is from here: https://aqs.epa.gov/aqsweb/airdata/download_files.html"),
+      h5("Base Code and Code influence from Professor Andy Johnson, https://www.evl.uic.edu/aej/424/ (week 2)"),
+      h6("librarys used: shiny, shinydashboard, ggplot2, lubridate, DT, jpeg, grid, leaflet, scales, reshape, tidyr, readr"),
+      h5("Techniques and methods adapted from:"),
+      h6("  *https://rstudio.github.io/shinydashboard/appearance.html"),
+      h6("  *reshape and melt function from https://www.statmethods.net/management/reshape.html"),
+      h6("  *https://stat.ethz.ch/R-manual/R-devel/library/base/html/toString.html"),
+      h6("  *https://stat.ethz.ch/R-manual/R-devel/library/base/html/strsplit.html "),
+      h6("  *https://stat.ethz.ch/R-manual/R-devel/library/base/html/sort.html"),
+      h6("  *https://stackoverflow.com/questions/52544228/r-shiny-display-static-text-outside-sidebar-panel"),
+      h6("  *https://stackoverflow.com/questions/12280571/how-can-i-remove-rows-containing-0-of-certain-columns-while-keeping-the-rows-i"),
+      h6("  *https://www.rdocumentation.org/packages/ggplot2/versions/3.1.0/topics/position_stack"),
+      h6("  *http://www.sthda.com/english/wiki/ggplot2-pie-chart-quick-start-guide-r-software-and-data-visualization"),
+      h6("  *https://stackoverflow.com/questions/26869141/conditionally-hiding-data-labels-in-ggplot2-graph"),
+      h6("  *https://www.displayr.com/how-to-make-a-pie-chart-in-r/"),
+      h6("  *https://stackoverflow.com/questions/38126212/how-to-check-if-data-table-has-empty-rows"),
+      h6("  *http://www.cookbook-r.com/Graphs/Bar_and_line_graphs_(ggplot2)/"),
+      h6("  *https://rpubs.com/euclid/343644"),
+      h6("  *http://shiny.rstudio.com/articles/shinyapps.html"),
+      h6("  *https://shiny.rstudio.com/reference/shiny/0.14/updateSelectInput.html"),
+      h6("  *https://gist.github.com/aagarw30/d08c5fb1794cf9b58fa38342db97b697"),
+      h6("  *http://shiny.rstudio.com/articles/shinyapps.html"),
+      h6("  *student who was in office hours on Friday - never got your name"),
+      h6("  *http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/"),
+      h6("  *https://stackoverflow.com/questions/33266157/how-to-add-more-whitespace-to-the-main-panel-in-shiny-dashboard"),
+      h6("  *professor Andy Johnson")
+      
+    ),
+    tabItem(
+      tabName = "boxes",
+      verbatimTextOutput("NO2"),
+      verbatimTextOutput("OZONE"),
+      verbatimTextOutput("CO"),
+      verbatimTextOutput("H2S"),
+      verbatimTextOutput("SO2"),
+      verbatimTextOutput("PM10"),
+      verbatimTextOutput("PM25"),
+      verbatimTextOutput("TEMPERATURE"),
+      verbatimTextOutput("HUMIDITY"),
+      verbatimTextOutput("INTENSITY")
+      
+      
+    )
     )
   ))
 
@@ -76,8 +149,37 @@ ui <- dashboardPage(
 # Sensors - Column Names
 # "uom"        "path"       "min"        "max"        "data_sheet"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output,session) {
   
   autoInvalidate <- reactiveTimer(1000*60)
   
@@ -91,8 +193,8 @@ server <- function(input, output) {
   getNodeData2 <- function(vsn, d,h){
     url <- "https://api.arrayofthings.org/api/observations?location=chicago&node="
     url <- paste(url, vsn,"&timestamp=","ge:2018-08-01T00:00:00&size=50000", sep="")
-    s <- download.file(url, "/var/tmp/obs", quiet = FALSE) 
-    t = fromJSON("/var/tmp/obs")
+    s <- download.file(url, "obs.html", quiet = FALSE) 
+    t = fromJSON("obs.html")
     u = t$data
     currentTime = Sys.time();
     gmtTime = as.POSIXlt(currentTime, tz="UTC")
@@ -132,6 +234,180 @@ server <- function(input, output) {
                         "chemsense.sht25.humidity")
   intensity_path = "chemsense.si1145.visible_light_intensity"
   
+  ################### check boxes ##############
+  
+  output$NO2 <- renderText({ input$NO2 })
+  output$OZONE <- renderText({ input$OZONE })
+  output$CO <- renderText({ input$CO })
+  output$H2S <- renderText({ input$H2S })
+  output$SO2 <- renderText({ input$SO2 })
+  output$PM10 <- renderText({ input$PM10 })
+  output$PM25 <- renderText({ input$PM25 })
+  output$TEMPERATURE <- renderText({ input$TEMPERATURE })
+  output$HUMIDITY <- renderText({ input$HUMIDITY })
+  output$INTENSITY <- renderText({ input$INTENSITY })
+  
+  
+  observeEvent(input$NO2, {
+    print('NO2 changed')
+    no2_IsSelected <- input$NO2
+  })
+  
+  observe({
+    no2_IsSelected <- input$NO2
+    if(input$NO2 == TRUE){
+      print('NO2 is true')
+    }
+    if(input$NO2 == FALSE){
+      print('NO2 is false')
+    }
+  })
+  
+  observeEvent(input$OZONE, {
+    print('OZONE changed')
+    ozone_IsSelected <- input$OZONE
+  })
+  
+  observe({
+    ozone_IsSelected <- input$OZONE
+    if(input$OZONE == TRUE){
+      print('OZONE is true')
+    }
+    if(input$OZONE == FALSE){
+      print('OZONE is false')
+    }
+  })
+  
+  observeEvent(input$CO, {
+    print('CO changed')
+    co_IsSelected <- input$CO
+  })
+  
+  observe({
+    co_IsSelected <- input$CO
+    if(input$CO == TRUE){
+      print('CO is true')
+    }
+    if(input$CO == FALSE){
+      print('CO is false')
+    }
+  })
+  
+  observeEvent(input$H2S, {
+    print('H2S changed')
+    h2s_IsSelected <- input$H2S
+  })
+  
+  observe({
+    h2s_IsSelected <- input$H2S
+    if(input$H2S == TRUE){
+      print('H2S is true')
+    }
+    if(input$H2S == FALSE){
+      print('H2S is false')
+    }
+  })
+  
+  observeEvent(input$SO2, {
+    print('SO2 changed')
+    so2_IsSelected <- input$SO2
+  })
+  
+  observe({
+    so2_IsSelected <- input$SO2
+    if(input$SO2 == TRUE){
+      print('SO2 is true')
+    }
+    if(input$SO2 == FALSE){
+      print('SO2 is false')
+    }
+  })
+  
+  observeEvent(input$PM10, {
+    print('PM10 changed')
+    pm10_IsSelected <- input$PM10
+  })
+  
+  observe({
+    pm10_IsSelected <- input$PM10
+    if(input$PM10 == TRUE){
+      print('PM10 is true')
+    }
+    if(input$PM10 == FALSE){
+      print('PM10 is false')
+    }
+  })
+  
+  observeEvent(input$PM25, {
+    print('PM25 changed')
+    pm25_IsSelected <- input$PM25
+  })
+  
+  observe({
+    pm25_IsSelected <- input$PM25
+    if(input$PM25 == TRUE){
+      print('PM25 is true')
+    }
+    if(input$PM25 == FALSE){
+      print('PM25 is false')
+    }
+  })
+  
+  observeEvent(input$TEMPERATURE, {
+    print('TEMPERATURE changed')
+    tempertature_IsSelected <- input$TEMPERATURE
+  })
+  
+  observe({
+    tempertature_IsSelected <- input$TEMPERATURE
+    if(input$TEMPERATURE == TRUE){
+      print('TEMPERATURE is true')
+    }
+    if(input$TEMPERATURE == FALSE){
+      print('TEMPERATURE is false')
+    }
+  })
+  
+  observeEvent(input$HUMIDITY, {
+    print('HUMIDITY changed')
+    humidity_IsSelected <- input$HUMIDITY
+  })
+  
+  observe({
+    humidity_IsSelected <- input$HUMIDITY
+    if(input$HUMIDITY == TRUE){
+      print('HUMIDITY is true')
+    }
+    if(input$HUMIDITY == FALSE){
+      print('HUMIDITY is false')
+    }
+  })
+  
+  observeEvent(input$INTENSITY, {
+    print('INTENSITY changed')
+    intensity_IsSelected <- input$INTENSITY
+  })
+  
+  observe({
+    intensity_IsSelected <- input$INTENSITY
+    if(input$INTENSITY == TRUE){
+      print('INTENSITY is true')
+    }
+    if(input$INTENSITY == FALSE){
+      print('INTENSITY is false')
+    }
+  })
+  
+  # no2_IsSelected <- reactive(input$NO2)
+  # ozone_IsSelected <- reactive(input$OZONE)
+  # co_IsSelected <- reactive( input$CO)
+  # h2s_IsSelected <- reactive( input$H2S)
+  # so2_IsSelected <- reactive( input$SO2)
+  # pm10_IsSelected <- reactive( input$PM10)
+  # pm25_IsSelected <- reactive( input$PM25)
+  # tempertature_IsSelected <- reactive( input$TEMPERATURE)
+  # humidity_IsSelected <- reactive( input$HUMIDITY)
+  # intensity_IsSelected <- reactive( input$INTENSITY)
   
   # Use these for the check boxes
   no2_IsSelected = TRUE
@@ -170,8 +446,8 @@ server <- function(input, output) {
     }
     url <- "https://api.arrayofthings.org/api/observations?location=chicago&node="
     url <- paste(url, vsn,"&timestamp=","ge:2018-08-01T00:00:00&sensor=", path,"&size=",size,sep="")
-    s <- download.file(url, "/var/tmp/obs", quiet = FALSE) 
-    t = fromJSON("/var/tmp/obs")
+    s <- download.file(url, "obs.html", quiet = FALSE) 
+    t = fromJSON("obs.html")
     u = t$data
     currentTime = Sys.time();
     gmtTime = as.POSIXlt(currentTime, tz="UTC")
@@ -184,10 +460,6 @@ server <- function(input, output) {
     }
     return(select(subset(u, as.POSIXlt(timestamp, tz="UTC", "%Y-%m-%dT%H:%M") %within% int), 'node_vsn', 'sensor_path', 'timestamp', 'value'))
   }
-  
-  
-  
-  
   
   
   getPollutantPaths <- function(){
@@ -205,6 +477,23 @@ server <- function(input, output) {
     return(pathList)
   }
   
+  
+ ################################################### origional 
+  # getPollutantPaths <- function(){
+  #   pathList = list()
+  #   if(no2_IsSelected){pathList = c(pathList, no2_path)}
+  #   if(ozone_IsSelected){pathList = c(pathList, ozone_path)}
+  #   if(co_IsSelected){pathList = c(pathList, co_path)}
+  #   if(h2s_IsSelected){pathList = c(pathList, h2s_path)}
+  #   if(so2_IsSelected){pathList = c(pathList, so2_path)}
+  #   if(pm10_IsSelected){pathList = c(pathList, pm10_paths)}
+  #   if(pm25_IsSelected){pathList = c(pathList, pm25_paths)}
+  #   if(tempertature_IsSelected){pathList = c(pathList, temperature_paths)}
+  #   if(humidity_IsSelected){pathList = c(pathList, humidity_paths)}
+  #   if(intensity_IsSelected){pathList = c(pathList, intensity_path)}
+  #   return(pathList)
+  # }
+  # 
   pollutantPaths <- reactive({
     getPollutantPaths()
   })
